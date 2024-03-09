5fc34344bc07b2306183ae429993d9703131f764
yihhao wang <2633565580@qq.com>
2024-03-09 17:01:22 +0800

    feat(player): supported converting klei ids to player.txt

--- player.go
+++ player.go
@@ -1,6 +1,9 @@
 package modparser
 
-import "strings"
+import (
+	"strings"
+	"unsafe"
+)
 
 // ParsePlayerTxt parse player.txt file, returns slice of klei ID
 func ParsePlayerTxt(content string) []string {
@@ -8,4 +11,10 @@ func ParsePlayerTxt(content string) []string {
 		return nil
 	}
 	return strings.Split(content, "\n")
+}
+
+// ToPlayerTxt converts kleiIDs to player.txt
+func ToPlayerTxt(kleiIDs []string) ([]byte, error) {
+	joinStr := strings.Join(kleiIDs, "\n")
+	return unsafe.Slice(unsafe.StringData(joinStr), len(joinStr)), nil
 }
